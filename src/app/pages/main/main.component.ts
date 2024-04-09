f048446d3fe58e1b9db7e1636c9ad21ab7297f33
Artem Hrishyn <artem.hrishyn.it@gmail.com>
2024-04-09 15:02:06 +0300

    add main page Part2

--- src/app/pages/main/main.component.ts
+++ src/app/pages/main/main.component.ts
@@ -1,22 +1,45 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { DataCollectionService } from '../../services/firebase/data-collection.service';
+import { DataServ } from '../../interfaces/dataServece.interfaces';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'bcs-main',
   standalone: true,
-  imports: [],
+  imports: [
+    CommonModule
+  ],
   templateUrl: './main.component.html',
   styleUrl: './main.component.scss'
 })
 
-export class MainComponent {
+export class MainComponent implements OnInit {
+
+  public dataServ: DataServ[] = [];
+  public image: string = '';
+  public title: string = '';
+  public text: string = '';
+
   constructor(
     private collectionService: DataCollectionService
-  )
-  {
-    this.collectionService.getDataServece().subscribe((data: any) => {
-      console.log(data);
+  ){}
+  ngOnInit(): void {
+    this.collectionService.getDataServece().subscribe((data: DataServ[]) => {
+      this.dataServ = data;
+
+      this.image = data[0].image;
+      this.title = data[0].title;
+      this.text = data[0].text
+      data.forEach((elem: DataServ) => {
+      });
     })
+  }
+
+  public clickService(value: DataServ){
+    console.log(value);
+    this.image = value.image;
+    this.title = value.title;
+    this.text = value.text;
   }
 }
 
